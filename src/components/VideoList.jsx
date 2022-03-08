c9fa2b9ba5235c80ec15bb23cf2e8407aa89e32f
Cody Kazuma <kodalpha@Codys-MacBook-Pro.local>
2022-03-07 18:13:12 -0800

    modified videolist

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,43 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+// var VideoList = (props) => (
+//   <div className="video-list">
+//     <div>{props.Video[0]}</div>
+//     <div><h5><em>videoListEntry</em> what view goes here?</h5></div>
+//     <div><h5><em>videoListEntry</em> what view goes here?</h5></div>
+//     <div><h5><em>videoListEntry</em> what view goes here?</h5></div>
+//     <div><h5><em>videoListEntry</em> what view goes here?</h5></div>
+//   </div>
+// );
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(video =>
+      <VideoListEntry video={video} />
+    )}
   </div>
 );
+// {/* <Videos video={['someVideo', 'anotherVideo', 'videoThree', 'videoFour', 'videoFive']} /> */}
+
+// class VideoEntries extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div>{this.props.video}</div>
+//     );
+//   }
+// }
+
+// const Videos = (props) => (
+//   <div>
+//     {
+//       props.video.map(video => <VideoEntries videos={video} />)
+//     }
+//   </div>
+// );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
