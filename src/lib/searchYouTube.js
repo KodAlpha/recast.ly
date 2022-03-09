eb8623f9c618ab83832fa75971aa1364b3fefdf8
Cody Kazuma <kodalpha@Codys-MacBook-Pro.local>
2022-03-08 17:40:46 -0800

    modified search youtube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -4,8 +4,41 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
-var searchYouTube = (query, callback) => {
-  // TODO
+var searchYouTube = (query, callback, errorCB) => {
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: query,
+    contentType: 'application/json',
+    success: callback || function (data) {
+      console.log('sup!');
+    },
+    error: errorCB || function(error) {
+      console.error('chatterbox: Failed to fetch messages', error);
+    }
+  });
 };
 
+
 export default searchYouTube;
+
+
+/*---------------------------------------------------*/
+
+// $.get('https://app-hrsei-api.herokuapp.com/api/recastly/videos', {
+//   youtubeApiKey: YOUTUBE_API_KEY,
+//   q: query,
+//   part: 'snippet',
+//   type: 'video'
+// })
+//   .done((items) => {
+//     console.log('hello');
+//     if (callback) {
+//       callback(items);
+//     }
+//   })
+//   .fail(({responseJSON}) => {
+//     responseJSON.error.errors.forEach((err) =>
+//       console.error(err)
+//     );
+//   });
